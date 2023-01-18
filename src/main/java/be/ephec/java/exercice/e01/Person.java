86ec5e1fc2dd00534a5ead5de965bf2ddecadba2
frederic <ferrantbe@gmail.com>
2023-01-18 09:39:53 +0100

    ex01 - add getters to Person

--- src/main/java/be/ephec/java/exercice/e01/Person.java
+++ src/main/java/be/ephec/java/exercice/e01/Person.java
@@ -4,7 +4,16 @@ public class Person {
     public String lastName;
     public String firstName;
     public int yearOfBirth;
+
     public int age;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
 
     public void computeAge(int year) {
         age = year - yearOfBirth;
