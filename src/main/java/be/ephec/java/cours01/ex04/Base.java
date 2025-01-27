04f80cab5f904ef3add865fa089cad71610b3d0c
ferrant <fferrant@ephec.be>
2025-01-27 19:34:23 +0100

    après Cours01

---
+++ src/main/java/be/ephec/java/cours01/ex04/Base.java
@@ -0,0 +1,30 @@
+package be.ephec.java.cours01.ex04;
+
+import java.util.Scanner;
+
+
+public class Base {
+
+    private static int base;
+    private static int nombre;
+
+    public static void main(String[] arg) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Quel est la base? ");
+        base = scanner.nextInt();
+
+        System.out.printf("Valeur en base %d: ", base);
+
+        if (scanner.hasNextInt(base)) {
+            nombre = scanner.nextInt(base);
+
+            System.out.println();
+
+            System.out.printf("En base %d, ça donne %d.", base, nombre);
+        }
+        scanner.close();
+    }
+
+
+} 
